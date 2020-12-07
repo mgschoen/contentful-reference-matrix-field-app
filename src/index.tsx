a871bb1fa85b1faa088a06a7216e2641182e4117
Martin Schön <martin.schoen@spiegel.de>
2020-12-07 11:04:37 +0100

    Removed unneeded code

--- src/index.tsx
+++ src/index.tsx
@@ -16,12 +16,6 @@ import '@contentful/forma-36-react-components/dist/styles.css';
 import '@contentful/forma-36-fcss/dist/styles.css';
 import './index.css';
 
-import Config from './components/ConfigScreen';
-import EntryEditor from './components/EntryEditor';
-import Page from './components/Page';
-import Sidebar from './components/Sidebar';
-import Field from './components/Field';
-import Dialog from './components/Dialog';
 
 import LocalhostWarning from './components/LocalhostWarning';
 
@@ -38,29 +32,8 @@ if (process.env.NODE_ENV === 'development' && window.self === window.top) {
     // Dont forget to delete the file too :)
     const ComponentLocationSettings = [
       {
-        location: locations.LOCATION_APP_CONFIG,
-        component: <Config sdk={(sdk as unknown) as AppExtensionSDK} />
-      },
-      {
         location: locations.LOCATION_ENTRY_FIELD,
-        component: <Field sdk={(sdk as unknown) as FieldExtensionSDK} />
       },
-      {
-        location: locations.LOCATION_ENTRY_EDITOR,
-        component: <EntryEditor sdk={(sdk as unknown) as EditorExtensionSDK} />
-      },
-      {
-        location: locations.LOCATION_DIALOG,
-        component: <Dialog sdk={(sdk as unknown) as DialogExtensionSDK} />
-      },
-      {
-        location: locations.LOCATION_ENTRY_SIDEBAR,
-        component: <Sidebar sdk={(sdk as unknown) as SidebarExtensionSDK} />
-      },
-      {
-        location: locations.LOCATION_PAGE,
-        component: <Page sdk={(sdk as unknown) as PageExtensionSDK} />
-      }
     ];
 
     // Select a component depending on a location in which the app is rendered.
